1b74fa1986d4d787ff714702f7ffe2063bcb1493
Florian Boor <florian.boor@kernelconcepts.de>
2017-06-06 15:52:38 +0200

    SC165: Merge support for SC165 from original Beck IPC patch

---
+++ include/configs/beck_sc165.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2017 Beck IPC GmbH
+ * Copyright (C) 2017 kernel concepts GmbH
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __BECK_SC165_CONFIG_H
+#define __BECK_SC165_CONFIG_H
+
+#define PHYS_SDRAM_SIZE		SZ_512M
+#define BOARD_STR		"sc165"
+
+#include "beck_sc1x5.h"
+
+#endif
+
